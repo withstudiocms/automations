68156d9e9ca73cf81914657c822380022f8088f3
Adam Matthiesen <amatthiesen@outlook.com>
2025-07-16 16:48:57 -0700

    create new automation (#1)
    * create new automation
    
    * update

---
+++ .github/actions/announcement-generator/rollup.config.js
@@ -0,0 +1,16 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+const config = {
+  input: "src/index.js",
+  output: {
+    esModule: true,
+    file: "dist/index.js",
+    format: "es",
+    sourcemap: true,
+  },
+	external: ['tinyglobby'],
+  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+};
+
+export default config;
